43da8cfa2e05a79d49d56be44c0a48498b10150b
Ryan Morris <ryanmorris971@gmail.com>
2025-01-28 21:12:28 -0400

    end of class 28th  masonry

--- PhotoShare/Controllers/HomeController.cs
+++ PhotoShare/Controllers/HomeController.cs
@@ -1,70 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoShare.Data;
 using PhotoShare.Models;
 
 namespace PhotoShare.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly PhotoShareContext _context;
+
         // Constructor
-        public HomeController()
+        public HomeController(PhotoShareContext context)
         {
+            _context = context;
         }
 
         // Home page - ../ or ../Home/Index
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            // To-do: Entity Framework - fetch all photos
+            // get a list of photos
+            var photos = await _context.Photo.ToListAsync();
 
-
-            // Create a list of photos
-            List<Photo> photos = new List<Photo>();
-
-            // Create photo object
-            Photo photo1 = new Photo()
-            {
-                PhotoId = 1,
-                Description = "This is my cat",
-                ImageFilename = "cat.jpg",
-                IsVisible = true
-            };
-
-            // Create photo object
-            Photo photo2 = new Photo();
-
-            photo2.PhotoId = 2;
-            photo2.Description = "This is my dog";
-            photo2.ImageFilename = "dog.jpg";
-            photo2.IsVisible = true;
-
-            // Create photo object
-            Photo photo3 = new Photo();
-
-            photo3.PhotoId = 3;
-            photo3.Description = "This is my hamster";
-            photo3.ImageFilename = "hamster.jpg";
-            photo3.IsVisible = true;
-
-            // Add them to the list
-            photos.Add(photo1);
-            photos.Add(photo2);
-            photos.Add(photo3);
 
             return View(photos);
         }
 
         // Display a photo by id - ../Home/DisplayPhoto/
-        public IActionResult PhotoDetails(int id)
+        public async Task<IActionResult> PhotoDetails(int id)
         {
             // To-do: Entity Framework - fetch the photo by id
-
-            Photo photo = new Photo()
-            {
-                PhotoId = id,
-                //Title = "Bird",
-                Description = "This is my bird",
-                ImageFilename = "bird.jpg",
-                IsVisible = true
-            };
+            var photo = await _context.Photo.FirstOrDefaultAsync(m => m.PhotoId == id);
 
             return View(photo);
         }
